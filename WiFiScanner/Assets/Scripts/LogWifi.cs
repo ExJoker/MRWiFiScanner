3e09c18f5bde82ca0588c18ca18288fa3d0403b6
Andreas Bresser <self@andreasbresser.de>
2018-03-07 15:09:16 +0100

    first commit - Search for WiFi APs and showing them using internal UWP functions

---
+++ WiFiScanner/Assets/Scripts/LogWifi.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogWifi : MonoBehaviour {
+    public Text DebugText;
+    // Use this for initialization
+    private static LogWifi Instance;
+    public int maxLogLines = 13;
+    private static List<string> logList = new List<string>();
+    private static List<string> newLogList = new List<string>();
+
+    void Start()
+    {
+        DebugText = DebugText == null ? GetComponent<Text>() : DebugText;
+        Instance = this;
+        DebugText.text = "";
+    }
+
+    public static void Log(string txt)
+    {
+        Debug.Log(txt);
+        newLogList.Add(txt);
+    }
+    
+    void Update()
+    {
+        if (newLogList != null && DebugText != null && newLogList.Count > 0)
+        {
+            logList.AddRange(newLogList);
+            newLogList.Clear();
+            if (logList.Count > maxLogLines)
+            {
+                logList.RemoveRange(0, logList.Count - maxLogLines);
+            }
+
+            DebugText.text = string.Join("\n", logList.ToArray());
+        }
+    }
+}
---
+++ WiFiScanner/Assets/Scripts/WiFiScan.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+#if WINDOWS_UWP
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Devices.WiFi;
+using Windows.Devices.Enumeration;
+#endif
+
+// get all ssids of all available WiFi networks
+// roughly based on https://www.oshyn.com/Blogs/2016/October/Accessing-Hololens-Wifi-Adapter
+
+public class WiFiScan : MonoBehaviour {
+#if WINDOWS_UWP
+    private WiFiAdapter adapter;
+    private bool ready = true;
+
+    private void GenerateNetworkReport(WiFiNetworkReport report) {
+        var networks = new List<string>();
+        foreach (var network in report.AvailableNetworks)
+        {
+            LogWifi.Log(string.Format("SSID: {0} -- SignalBars: {1} -- Db: {2} -- Mac: {3}", 
+network.Ssid, network.SignalBars, network.NetworkRssiInDecibelMilliwatts, network.Bssid));
+        }
+        LogWifi.Log("found " + report.AvailableNetworks.Count + " APs");
+    }
+
+    public async void Scan() {
+        if (ready) {
+            ready = false;
+            var result = await WiFiAdapter.FindAllAdaptersAsync();
+            if (result.Count >= 1)
+            {
+                var firstAdapter = result[0];
+                await firstAdapter.ScanAsync();
+                GenerateNetworkReport(firstAdapter.NetworkReport);
+            }
+            ready = true;
+            Scan();
+        }
+        
+    }
+#endif
+
+    void Start ()
+    {
+#if WINDOWS_UWP
+        LogWifi.Log("Looking for WiFi APs");
+        Scan();
+#else
+        LogWifi.Log("sorry, this only works as UWP-build");
+#endif
+    }
+}
